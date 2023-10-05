ef01c350ad5973f4c688a07310c4ac34985d2fea
denerbatista <denerbatista@icloud.com>
2023-10-04 21:50:23 -0300

    feat: config validation auth create

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const port = 3000 || process.env.PORT
 
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder().setTitle('Nest-Project').setVersion('1.0.0').addServer('http://localhost:3000', 'Local').addTag('Status').addTag('Auth').build();
 
