f8bbcb499bf921a1f8de49a04e93b6a1538fe8a0
denerbatista <denerbatista@icloud.com>
2023-10-16 20:58:25 -0300

    feat: add controller auth and local-auth.guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UserModule } from 'src/user/user.module';
 import { UserService } from 'src/user/user.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 
