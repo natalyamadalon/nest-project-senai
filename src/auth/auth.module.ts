22ad3afa7fb2cb052a940fc25fcaed750f18868e
denerbatista <denerbatista@icloud.com>
2023-10-16 20:34:37 -0300

    refactor(auth): add error and add type in validateUser

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
+import { UserService } from 'src/user/user.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
 
 @Module({
-  imports: [UserModule],
-  providers: [AuthService]
+  imports: [PrismaModule],
+  providers: [AuthService, UserService]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,14 +4,23 @@ import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UserService) {}
+  constructor(private readonly usersService: UserService) {}
 
-  async validateUser(email: string, password: string): Promise<any> {
-    const user: User  = await this.usersService.findOne(email);
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<{
+    id: string;
+    email: string;
+    name: string;
+    picture: string;
+    admin?: boolean;
+  } | null> {
+    const user: User = await this.usersService.findOne(email);
     if (user && user.password === password) {
       const { password, ...result } = user;
       return result;
     }
-    return null;
+    throw Error("Senha incorreta!");
   }
 }
