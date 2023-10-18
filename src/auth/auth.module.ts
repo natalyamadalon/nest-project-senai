18554a96f4b80a8081cbd88b917bc59043140be0
denerbatista <denerbatista@icloud.com>
2023-10-17 21:41:57 -0300

    refactor: change payload and expiresIn

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { AuthController } from './auth.controller';
     PassportModule,
     JwtModule.register({
       secret: process.env.SECRET,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '2h' },
     }),
   ],
   controllers: [AuthController],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -57,7 +57,7 @@ export class AuthService {
   }
 
   async login(user: User) {
-    const payload = { email: user.email, sub: user.id };
+    const payload = user;
     return {
       access_token: this.jwtService.sign(payload),
     };
