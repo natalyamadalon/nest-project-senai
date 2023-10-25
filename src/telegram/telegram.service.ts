3f22e65203ebac6e21c8b0115a45ace18531b80a
natalyamadalon <pereiramadalon@gmail.com>
2023-10-25 20:50:06 -0300

    feat: add service  sendTelegrafmedia

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import {Telegraf} from 'telegraf';
+import { Telegraf } from 'telegraf';
+import { MediaGroup } from 'telegraf/typings/telegram-types';
 
 @Injectable()
 export class TelegramService {
@@ -11,7 +12,14 @@ export class TelegramService {
   //   this.chatId = '-1002033466946';
   // }
 
-  async sendTelegrafText({message, chatid, tokenbot }:{message: string, chatid: number, tokenbot: string }) {
+  base64ToImage (base64: string): Buffer { 
+    const base64Data = base64.replace(/^data:image\/jpeg;base64,/, '') 
+
+    return Buffer.from(base64Data)
+  }
+
+
+  async sendTelegrafText({ message, chatid, tokenbot }: { message: string, chatid: number, tokenbot: string }) {
     try {
       const bot = new Telegraf(tokenbot);
       await bot.telegram.sendMessage(chatid, message);
@@ -19,5 +27,25 @@ export class TelegramService {
     } catch (error) {
       console.log("sendTelegrafText: ", error)
     }
+  }
+
+
+  async sendTelegrafmedia(
+    message: string,
+    chatid: string,
+    tokenbot: string,
+    images: string[]
+  ) {
+    const bot = new Telegraf(tokenbot);
+    const media: MediaGroup = images.map((image) => ({
+      type: 'photo',
+      media: { source: this.base64ToImage(image) },
+      caption: '',
+    }));
+
+    media[media.length -1].caption = message
+
+    await bot.telegram.sendMediaGroup(chatid, media)
+    console.log('Mensagem enviada')
   }
 }
