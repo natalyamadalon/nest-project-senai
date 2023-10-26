78e9f8b6867c09eba75d6f79d837069030e7b8b6
natalyamadalon <pereiramadalon@gmail.com>
2023-10-26 20:42:38 -0300

    doc: add example in SendMessageDto

--- src/telegram/dto/sendMessage.dto.ts
+++ src/telegram/dto/sendMessage.dto.ts
@@ -1,19 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class SendMessageDto {
-  @ApiProperty()
+  @ApiProperty({example: 'Olá'})
   message: string;
 
-  @ApiProperty()
+  @ApiProperty({example: -1002033466946})
   chatid: number;
 
-  @ApiProperty()
+  @ApiProperty({example: "6827435531:AAHgj-zB8zEenaJRUnrhRU0PBmmnxZUeQDU"})
   tokenbot: string;
 
-  @ApiProperty()
+  @ApiProperty({example: []})
   images: string [] | undefined
 
-  @ApiProperty()
+  @ApiProperty({example: "2023-10-26T20:33:00"})
   schedule: string | Date
 
 }
